b3550d238a5be21923f2f8102ee7b4422d4fdfb3
peipeiziyapeizizi <78746199+peipeiziyapeizizi@users.noreply.github.com>
2021-02-24 21:34:37 +0800

    Add files via upload

--- wc.py
+++ wc.py
@@ -1,16 +1,18 @@
 import requests
 import csv
 import jieba
+import numpy
+from PIL import Image
 from lxml import etree
 from wordcloud import WordCloud
 
-
-for page in range(0,226,25):
+for page in range(0,220,20):
     url = 'https://movie.douban.com/subject/34779692/comments?start=%d&limit=20&status=P&sort=new_score'%page
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
     response = requests.get(url=url,headers=headers).content.decode('utf-8')
     soup = etree.HTML(response)
     paqu = soup.xpath('//p[@class=" comment-content"]/span[@class="short"]/text()')
+    print(paqu)
     with open('豆瓣.csv',mode='a',encoding='utf-8',newline="") as f:
         csv_write = csv.writer(f)
         csv_write.writerow(paqu)
@@ -19,7 +21,6 @@ text = open(r'.\豆瓣.csv',encoding = 'utf_8').read()
 
 cut_text = jieba.cut(text)
 result = ' '.join(cut_text)
-
 
 color_mask = numpy.array(Image.open('p.png'))
 
@@ -34,5 +35,3 @@ wc = WordCloud(
 )
 wc.generate(result)
 wc.to_file(r'.\wordcloud.png')
-
-
